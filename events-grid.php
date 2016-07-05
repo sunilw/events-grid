ffc27e36ca04b02d91650671443556d13e91ca2f
Sunil Williams <sunil@sunil.co.nz>
2016-07-05 14:03:28 +1200

    replace isotope library with isotope jquery plugin

--- events-grid.php
+++ events-grid.php
@@ -1,56 +1,53 @@
 <?php
 
 /*
-Plugin Name: Events Grid
-Description: Isotope powered layout for Modern Tribe's Calendar Events
-Version: 0.1.0
-Plugin URI: http://sunil.con.z
-Author URI: http://sunil.co.nz
-Author: Sunil Williams
-*/
+   Plugin Name: Events Grid
+   Description: Isotope powered layout for Modern Tribe's Calendar Events
+   Version: 0.1.0
+   Plugin URI: http://sunil.con.z
+   Author URI: http://sunil.co.nz
+   Author: Sunil Williams
+ */
 
 /*
-* If the shortcode is present in the page, then continue. Otherwise, just return
-*/
+ * If the shortcode is present in the page, then continue. Otherwise, just return
+ */
 
 /*
-* Set up constants
-*/
+ * Set up constants
+ */
 defined( 'ABSPATH' ) or die( 'naff off' );
 define( "SWEG_PLUGINDIRURI",  plugin_dir_url(__FILE__) ) ;
 define( "SWEG_PLUGINDIR",  plugin_dir_path(__FILE__) ) ;
 
 
 /**
-* conditionally load Isotope
-*/
+ * conditionally load Isotope
+ */
 function sweg_load_assets() {
-global $post;
-if  (has_shortcode( $post->post_content, 'events-grid') ) {
-wp_enqueue_script('Isotope', '//npmcdn.com/isotope-layout@3.0.1/dist/isotope.pkgd.js', array(), null  , false );
-wp_enqueue_script('Isotope', '//npmcdn.com/isotope-layout@3.0.1/dist/isotope.pkgd.js', array(), null  , false );
-wp_enqueue_style('events-grid-styles',  SWEG_PLUGINDIRURI . "/css/events-grid.css"  ) ;
-}
+    global $post;
+    if  (has_shortcode( $post->post_content, 'events-grid') ) {
+	// wp_enqueue_script('Isotope', '//npmcdn.com/isotope-layout@3.0.1/dist/isotope.pkgd.js', array(), null  , false );
+        wp_enqueue_script('jquery-Isotope', '//cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.1/isotope.pkgd.min.js' , array('jquery'), null  , false );
+        wp_enqueue_style('events-grid-styles',  SWEG_PLUGINDIRURI . "/css/events-grid.css"  ) ;
+    }
 }
 add_action( 'wp_enqueue_scripts', 'sweg_load_assets');
 
 /*
-* Inject the script into the footer
-*/
+ * Inject the script into the footer
+ */
 function sweg_init_isotope() {
-global $post;
-if  (has_shortcode( $post->post_content, 'events-grid') ) {
+    global $post;
+    if  (has_shortcode( $post->post_content, 'events-grid') ) {
 
-    ?>
-    <script>
-     $('.grid').isotope({
-         itemSelector: '.grid-item',
-         layoutMode: 'fitRows'
-     });
+?>
+    <script>                    
+
     </script>
 <?php           }
 }
-   add_action('wp_footer', 'sweg_init_isotope');
+add_action('wp_footer', 'sweg_init_isotope');
 
 
 
@@ -58,7 +55,7 @@ if  (has_shortcode( $post->post_content, 'events-grid') ) {
 
 add_shortcode(   'events-grid',  'sw_events_grid_loop' ) ;
 function sw_events_grid_loop() {
-$events_loop = include (SWEG_PLUGINDIR  . '/loop/grid_loop.php' ) ;
-echo $html  ;
-return ;
+    $events_loop = include (SWEG_PLUGINDIR  . '/loop/grid_loop.php' ) ;
+    echo $html  ;
+    return ;
 }
