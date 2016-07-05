0aa3e9d81acdba9d8515c97ed6ade48fa49fe2dd
Sunil Williams <sunil@sunil.co.nz>
2016-07-05 13:08:56 +1200

    add istope init script to page footer

--- events-grid.php
+++ events-grid.php
@@ -1,35 +1,64 @@
 <?php
 
 /*
-   Plugin Name: Events Grid
-   Description: Isotope powered layout for Modern Tribe's Calendar Events
-   Version: 0.1.0
-   Plugin URI: http://sunil.con.z
-   Author URI: http://sunil.co.nz
-   Author: Sunil Williams
- */
+Plugin Name: Events Grid
+Description: Isotope powered layout for Modern Tribe's Calendar Events
+Version: 0.1.0
+Plugin URI: http://sunil.con.z
+Author URI: http://sunil.co.nz
+Author: Sunil Williams
+*/
 
 /*
- * Set up constants
- */
+* If the shortcode is present in the page, then continue. Otherwise, just return
+*/
+
+/*
+* Set up constants
+*/
 defined( 'ABSPATH' ) or die( 'naff off' );
 define( "SWEG_PLUGINDIRURI",  plugin_dir_url(__FILE__) ) ;
 define( "SWEG_PLUGINDIR",  plugin_dir_path(__FILE__) ) ;
 
+
 /**
- * conditionally load Isotope
- */
+* conditionally load Isotope
+*/
 function sweg_load_assets() {
-    global $post;
-    if  (has_shortcode( $post->post_content, 'events-grid') ) {
-        wp_enqueue_script('Isotope', '//npmcdn.com/isotope-layout@3.0.1/dist/isotope.pkgd.js'  );
-    }
+global $post;
+if  (has_shortcode( $post->post_content, 'events-grid') ) {
+wp_enqueue_script('Isotope', '//npmcdn.com/isotope-layout@3.0.1/dist/isotope.pkgd.js', array(), null  , false );
+wp_enqueue_script('Isotope', '//npmcdn.com/isotope-layout@3.0.1/dist/isotope.pkgd.js', array(), null  , false );
+wp_enqueue_style('events-grid-styles',  SWEG_PLUGINDIRURI . "/css/events-grid.css"  ) ;
+}
 }
 add_action( 'wp_enqueue_scripts', 'sweg_load_assets');
 
+/*
+* Inject the script into the footer
+*/
+function sweg_init_isotope() {
+global $post;
+if  (has_shortcode( $post->post_content, 'events-grid') ) {
+
+    ?>
+    <script>
+     $('.grid').isotope({
+         itemSelector: '.grid-item',
+         layoutMode: 'fitRows'
+     });
+    </script>
+<?php           }
+}
+   add_action('wp_footer', 'sweg_init_isotope');
+
+
+
+
+
 add_shortcode(   'events-grid',  'sw_events_grid_loop' ) ;
 function sw_events_grid_loop() {
-    $events_loop = include (SWEG_PLUGINDIR  . '/loop/grid_loop.php' ) ;
-    echo $html  ;
-    return ;
+$events_loop = include (SWEG_PLUGINDIR  . '/loop/grid_loop.php' ) ;
+echo $html  ;
+return ;
 }
--- loop/grid_loop.php
+++ loop/grid_loop.php
@@ -1,50 +1,56 @@
 <?php
 
 $args = array() ;
-
 $events = tribe_get_events( $args );
 
 ?>
 
 <div class="sw-events-grid">
 
-<?php  
-foreach ($events as $event ) { ?>    
+    <?php
+    foreach ($events as $event ) { ?>
 
-    <div class="debug">
-	<?php //    var_dump(get_post_meta($event->ID))   ?>
-	
-	<?php 
-	/***
-	* get the venue metadata	   
-	*/
-	$venue_id = $event->_EventVenueID ;
-	$venue = get_post_meta($venue_id) ;
-	// var_dump($venue) ;
-	?>	
-    </div>
+        <div class="debug">
+            <?php //    var_dump(get_post_meta($event->ID))   ?>
+
+            <?php
+
+            $event_meta = get_post_meta($event->ID) ;
+
+            // var_dump($event_meta) ;
+
+            // get the venue metadata
+            $venue_id = $event->_EventVenueID ;
+            $venue = get_post_meta($venue_id) ;
+            // var_dump($venue) ;
+            ?>
+        </div>
 
 
-    <div class="event-card">
-	<h3><?php echo $event->post_title  ?></h3>
-	
-	<div class="event-details">
-	    
-	    <div class="event-location">
-		<h4>Location: <?php  echo $venue['_VenueCity'][0] ;  ?></h4>
-	    </div>
-	    
-	    <div class="event-excerpt">
-		<?php    echo $event->post_excerpt;  ?>
-	    </div>
-	</div>
-	
-    </div> <!-- ENDS .event-card -->
+        <div class="event-card">
+            <h3><?php echo $event->post_title  ?></h3>
 
-<?php }  ?>
+            <div class="event-details">
+
+                <div class="event-location">
+                    <h4>City: <?php  echo $venue['_VenueCity'][0] ;  ?></h4>
+                </div>
+
+                <?php    if ( !empty($event_meta['_EventURL'][0]) ) {  ?>
+                    <div class="event-url">
+                        <p>Event Website:  <a href="<?php  echo $event_meta['_EventURL'][0]  ;  ?>"><?php  echo $event_meta['_EventURL'][0]  ;  ?></a></p>
+                    </div>
+                <?php  } ?>
+
+                <div class="event-excerpt">
+                    <?php    echo $event->post_excerpt;  ?>
+                </div>
+            </div>
+
+        </div> <!-- ENDS .event-card -->
+
+    <?php }  ?>
 
 
 
 </div>  <!-- ENDS .sw-events-grid -->
-
-
