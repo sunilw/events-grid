54bf8ae20124f2c9b272f6972def87196fb95303
Sunil Williams <sunil@sunil.co.nz>
2016-08-08 10:30:37 +1200

    add output for facebook page. depends on Advanced Feilds, in pro version

--- events-grid.php
+++ events-grid.php
@@ -27,7 +27,7 @@ define( "SWEG_PLUGINDIR",  plugin_dir_path(__FILE__) ) ;
 function sweg_load_assets() {
     global $post;
     if  (has_shortcode( $post->post_content, 'events-grid') ) {
-	// wp_enqueue_script('Isotope', '//npmcdn.com/isotope-layout@3.0.1/dist/isotope.pkgd.js', array(), null  , false );
+	//	wp_enqueue_script('Isotope', '//npmcdn.com/isotope-layout@3.0.1/dist/isotope.pkgd.js', array(), null  , false );
         wp_enqueue_script('jquery-Isotope', '//cdnjs.cloudflare.com/ajax/libs/jquery.isotope/3.0.1/isotope.pkgd.min.js' , array('jquery'), null  , false );
         wp_enqueue_style('events-grid-styles',  SWEG_PLUGINDIRURI . "/css/events-grid.css"  ) ;
     }
@@ -42,16 +42,15 @@ function sweg_init_isotope() {
     if  (has_shortcode( $post->post_content, 'events-grid') ) {
 
 ?>
-    <script>                    
-     $ = jQuery.noConflict()
-	 $('.sw-events-grid').isotope({ 
-	     itemSelector : '.event-card',	     
-	 }) ;
+    <script>
+     $ = jQuery.noConflict() ;
+     $('.sw-events-grid').isotope({ 
+	 itemSelector : '.event-card',	     
+     }) ;
     </script>
 <?php           }
 }
 add_action('wp_footer', 'sweg_init_isotope');
-
 
 add_shortcode(   'events-grid',  'sw_events_grid_loop' ) ;
 function sw_events_grid_loop() {
--- loop/grid_loop.php
+++ loop/grid_loop.php
@@ -1,14 +1,14 @@
 <?php
 
 $args = array(
-    category_in => 'day-of-action-september-2016'
+    //     category_in => 'day-of-action-september-2016'
 ) ;
 $events = tribe_get_events( $args );
 
 ?>
 
 <div class="sw-events-grid">
-
+    
     <?php
     foreach ($events as $event ) { ?>
 
@@ -21,57 +21,98 @@ $events = tribe_get_events( $args );
             $venue_id = $event->_EventVenueID ;
             $venue_meta = get_post_meta($venue_id) ;
             // var_dump($venue_meta) ;
+	    
+	    /*
+	     * get the custom fields
+	     */
+	    $cfields = tribe_get_custom_fields() ; 
+	    
             ?>
+
+	    
         </div>
 
-
         <div class="event-card">
+	    <?php global $post ;  ?>
+	    <?php  
+	    $event_fields = tribe_get_custom_fields($event->ID);
+	    // var_dump($event_fields) ;	    
+	    //var_dump($cfields) ;
+	    ?>
+	    
+	    <!-- <h1><?phpecho $event->ID ;	?></h1> -->
 
             <header>
                 <?php   if (  !empty($venue_meta['_VenueCity']) ) {   ?>
-                    <div class="city">
-                        <h2><?php  echo $venue_meta['_VenueCity'][0]  ?></h2>
-                    </div>
+                    <div class="venue-city"><h2><?php  echo $venue_meta['_VenueCity'][0]  ?></h2></div>
                 <?php }  ?>
             </header>
 
-	    <?php 
-	    /*
-	     * For our initial application we won't be using the post title.
-	     * But we will probably want to use it if we repurpose the plugin
-	     * <h3><?php echo $event->post_title ?></h3>
-	    */
-	    ?>
+            <?php
+            /*
+             * For our initial application we won't be using the post title.
+             * But we will probably want to use it if we repurpose the plugin
+             * <h3><?php echo $event->post_title ?></h3>
+             */
+            ?>
 
             <div class="event-details">
 
                 <?php    if ( !empty($venue_meta['_VenueVenue'][0]) ) {  ?>
                     <div class="venue">
+			<header class="event-meta-header venue"><span>venue</span></header>
                         <div class="event-url">
-                            <h3>
-                                <?php echo $venue_meta['_VenueVenue'][0]  ?>
-                            </h3>
+                            <h3><?php echo $venue_meta['_VenueVenue'][0]  ?></h3>
                         </div>
                     </div>
                 <?php  } ?>
 
-		<div class="event-start">
-		    <p>From <?php  echo tribe_get_start_date( $event->ID, false, "g:i a" ); ?></p>	    
+		<?php 
+		$organizer =  $cfields['event organizer'] ;
+		if ( isset($organizer) ) {		?>
+
+		<div class="organizer">
+		    <h5>organizer:</h5>
+		    <h2><?php  
+
+			echo $cfields['event organizer'] ;
+			?></h2>
 		</div>
 
-		<div class="event-link">
-		    <p><a href=" <?php    the_permalink($event->ID)   ?>">
-			More Information
-		    </a>
-		    </p>                   		    
-		</div>
+		<?php }  ?>
 
-	    </div>
-	    
-	
+                <?php
+                $event_start_time =  tribe_get_start_date( $event->ID, false, "g:i a" );
+		$event_end_time =  tribe_get_end_date( $event->ID, false, "g:i a" );
+		
+                if ( isset($event_start_time) ) {   ?>
+                    <div class="event-time">
+			<header class="event-meta-header time"><span>time</span></header>
+                        <p>
+			    <span class="start"><?php  echo $event_start_time ?></span>			    
+			    <?php  if ( isset($event_end_time) ) {   ?>
+				<span class="end"> to <?php echo $event_end_time  ?></span>
+				<?php }   ?>
+			</p>			
+                    </div>
+                <?php }  ?>
 
-	</div> <!-- ENDS .event-card -->
+		<?php  
+		$fbpage =  $event_fields['event facebook page'] ;
+		if (isset($fbpage))  { ?>
+		    <div class="event-facebook-page">
+			<p> <a href="<?php echo $fbpage  ?>" target="_blank"  >On facebook</a></p>
+		    </div>
+		<?php }  ?>
 
+                <div class="event-link">
+                    <p><a href=" <?php    the_permalink($event->ID)   ?>" class="btn btn-defaul">more information</a>
+                    </p>
+                </div>
+
+            </div>
+
+        </div> <!-- ENDS .event-card -->
     <?php }  ?>
 
 
